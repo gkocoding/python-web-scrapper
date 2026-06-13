af2577f21520ccd75c4cbf8c62e8fdaca8ffdb84
gkocoding <gosho.radew08@gmail.com>
2026-06-13 21:06:31 +0300

    Pagination Added
    Add pagination - scrape all 1000 books across all pages

--- scraper.py
+++ scraper.py
@@ -2,22 +2,27 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-response = requests.get('http://books.toscrape.com/')
-
-soup = BeautifulSoup(response.content, 'html.parser')
-
-books = soup.find_all('article', class_='product_pod')
-
+page_number = 1
 all_books = []
 
-for book in books:
-    title = book.find('h3').find('a').get('title')
-    price = book.find('p', class_='price_color').text
-    rating = book.find('p', class_='star-rating').get('class')[1]  # Get the second class which indicates the rating
-    dictionary = {'title': title, 'price': price, 'rating': rating}
-    all_books.append(dictionary)
+while True:
+    response = requests.get(f'http://books.toscrape.com/catalogue/page-{page_number}.html')
+    print(page_number)
+    if response.status_code != 200:
+        break
+    soup = BeautifulSoup(response.content, 'html.parser')
+    books = soup.find_all('article', class_='product_pod')
+    for book in books:
+        title = book.find('h3').find('a').get('title')
+        price = book.find('p', class_='price_color').text
+        rating = book.find('p', class_='star-rating').get('class')[1]  # Get the second class which indicates the rating
+        dictionary = {'title': title, 'price': price, 'rating': rating}
+        all_books.append(dictionary)
+    page_number += 1
+
+
     
-print(all_books)
+print(len(all_books))
 
 with open('books.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.DictWriter(f, fieldnames=['title', 'price', 'rating'])
