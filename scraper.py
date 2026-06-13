42c9431de684ca4a64be8288392cec00503d2f4a
gkocoding <gosho.radew08@gmail.com>
2026-06-13 20:42:49 +0300

    Update scraper.py
    Store book data in list of dictionaries

--- scraper.py
+++ scraper.py
@@ -7,10 +7,13 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 books = soup.find_all('article', class_='product_pod')
 
+all_books = []
+
 for book in books:
     title = book.find('h3').find('a').get('title')
     price = book.find('p', class_='price_color').text
     rating = book.find('p', class_='star-rating').get('class')[1]  # Get the second class which indicates the rating
-    print(title)
-    print(price)
-    print(rating)
+    dictionary = {'title': title, 'price': price, 'rating': rating}
+    all_books.append(dictionary)
+    
+print(all_books)
